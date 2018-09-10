1bef6b6ce19c5767f20c0ffd0ad0121e09feb8db
msaim2014 <msaim1998@gmail.com>
2018-09-10 05:06:32 -0400

    update

--- index/js/creative.js
+++ index/js/creative.js
@@ -2,7 +2,7 @@
     "use strict"; // Start of use strict
 
     // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $(document).on('click', 'a.page-scroll', function(event) {
+    $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: ($($anchor.attr('href')).offset().top - 50)
---
+++ index/js/style.js
@@ -0,0 +1,8 @@
+function myFunction() {
+      var ref = document.getElementById("references");
+      if (ref.style.display === "none") {
+          ref.style.display = "block";
+      } else {
+          ref.style.display = "none";
+      }
+}
